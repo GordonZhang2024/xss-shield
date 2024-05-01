957aa15ecb24907881a342416187ef2abe918e4a
Gordon Zhang <jp20171211@163.com>
2024-05-01 08:14:00 +0800

    Update __init__.py

--- src/xss_shield/__init__.py
+++ src/xss_shield/__init__.py
@@ -1,11 +1,11 @@
 def escape(s: str) -> str:
-    s = s.replace('<', '&lt')\
-         .replace('>', '&gt')\
-         .replace('&', '&amp')\
+    s = s.replace('<', '&lt;')\
+         .replace('>', '&gt;')\
+         .replace('&', '&amp;')\
          .replace(' ', '-')\
          .replace("'", "&#39;")\
          .replace('"', "&#34;")\
-         .replace(';', '&#59')\
-         .replace('/', '&#47')\
+         .replace(';', '&#59;')\
+         .replace('/', '&#47;')\
     
     return s
